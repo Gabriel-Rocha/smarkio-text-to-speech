b8a0a59a39e2ee0ad49bf8262ffa588a29814b92
Gabriel-Rocha <53313433+Gabriel-Rocha@users.noreply.github.com>
2021-04-01 13:12:15 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,23 +1,51 @@
-const express = require("express");
-const server = express();
-const bodyParser = require("body-parser");
-const cors = require('cors')
+const express = require('express');
+const app = express();
+const data = require('./data.json');
+
+app.use(express.json());
+
+app.get("/clients", function (req, res) {
+    res.json(data);
+});
+
+app.get("/clients/:id", function (req, res){
+    const {id} = req.params;
+    const client = data.find(cli => cli.id = id);
+    
+    if (!client) return res.status(204).json();
+    
+    res.json(client);
+});
 
 
-server.set('view engine', 'ejs');
-
-server.use(cors());
-
-server.use(express.static('public'));
-
-
-server.use(bodyParser.urlencoded({ extended: false }));
-server.use(bodyParser.json());
+app.post("/clients", function (req, res){
+    const {name, email} = req.body;
+        
+    res.json({name, email});
+});
 
 
-server.use('/', require('./routes/routes'));
+app.put("/clients/:id",function (req, res){
 
+    const {id} = req.params;
+    const client = data.find(cli => cli.id = id);
 
-server.listen(3000, () => {
-    console.log("O servidor está rodando!")
+    if (!client) return res.status(204).json();
+
+    const {name} = req.body;
+
+    client.name = name;
+
+    res.json(client);
+});
+
+app.delete("/clients/:id",function (req, res){
+    const {id} = req.params;
+    const clientsFiltered = data.filter(client => client.id != id);
+
+    res.json(clientsFiltered);
+});
+
+app.listen(3000, function() {
+    console.log('server is runing');
 });
